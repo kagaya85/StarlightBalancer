74bab6df1055f8ce08b046210aafc9e304547119
Kagaya <kagaya85@outlook.com>
2022-08-27 14:38:08 +0800

    update

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -10,3 +10,5 @@ make load-image
 kubectl apply -f "$dir"/config
 
 kubectl apply -f "$dir"/prometheus -f "$dir"/balancer-deploy.yaml -f "$dir"/services-deploy.yaml
+
+curl -sSL https://mirrors.chaos-mesh.org/v2.3.0/install.sh | bash
